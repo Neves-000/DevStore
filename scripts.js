a4467e56922ad1dba58c0e73cb236dd88ce367f5
Neves-000 <rayvinicius000@gmail.com>
2025-10-26 15:18:18 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Navegação suave
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({behavior: 'smooth'});
+  });
+});
